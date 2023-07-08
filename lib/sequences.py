7157c745c48829838cf2f487e3ef22588d88c19a
Stella Mazzillo <stellagress5@gmail.com>
2023-07-07 22:14:59 -0400

    complete p3 sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     ini_values = [0,1]
+
+#     if length == 0:
+#         ini_values=[]
+#     elif length == 1:
+#         ini_values=[0]
+#     print(ini_values)
+
+
+
+
 def print_fibonacci(length):
-    pass
+    ini_value = [0,1]
+
+    if length == 0:
+        ini_value = []
+    elif length == 1:
+        ini_value = [0]
+    elif length > 2 :
+        while len(ini_value) < length:
+            ini_value.append(ini_value[-1] + ini_value[-2])
+    print(ini_value)
+
+
+
+      
+
+
+
+
+
